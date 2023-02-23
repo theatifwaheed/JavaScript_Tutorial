50615eb3aa283ecd2dd1ef3963c0093712a34bba
TheAtifWaheed <theatifwaheed@gmail.com>
2023-02-23 13:16:45 +0500

    conditionalRendering

--- React/my-app/src/components/counter.jsx
+++ React/my-app/src/components/counter.jsx
@@ -9,28 +9,48 @@ class Counter extends Component {
     fontSize: 10,
     fontWeight: "bold",
   };
+
   render() {
     let classes = this.getClasses();
     return (
       <div>
         {/* <React.Fragment> */}
         {/* <h1>{this.state.count}</h1> */}
-        <span style={{ fontSize: 30 }} className={classes}>
+        <span style={{ fontSize: 15 }} className={classes}>
           {this.formatCounter()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
+        <button className="btn btn-secondary btn-lg">Increment</button>
+        <div>
+          {this.state.tags.length === 0 && "Please Create the TAG"}
+          {/* Shows the text if tags length = 0 |||| Last wale k bad jo ho ga wo print hoga... */}
+          {/* Condition_1 && Condition_2 && "Condition_3" => is me "Condition_3" Print hoga */}
+          {this.renderTags()}
+        </div>
+
+        {/* <ul>
           {this.state.tags.map((tag) => (
             <li key={tag}>{tag}</li>
           ))}
-        </ul>
+        </ul> */}
         {/* </React.Fragment> */}
       </div>
     );
   }
+
+  renderTags() {
+    if (this.state.tags.length === 0)
+      return <p style={this.styles}>There are no TAGS</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
   getClasses() {
-    let classes = "badge m-2 badge";
-    classes += this.state === 0 ? "warning" : "primary";
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
 
